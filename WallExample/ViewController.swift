97706fd064ecfea4f96cb48e396ca3be7f672e99
Walid Sahli <walidbinco@gmail.com>
2020-09-12 10:32:15 +0100

    Initial Commit

---
+++ WallExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallExample
+//
+//  Created by Mac on 12/09/2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
